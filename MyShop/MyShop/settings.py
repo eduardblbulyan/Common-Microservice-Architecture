7fe9a660bd63244246ac26a2864a411202ff37fb
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-07-25 11:54:14 +0400

    Adding elasticsearch_dsl config

--- MyShop/MyShop/settings.py
+++ MyShop/MyShop/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 
+ELASTICSAERCH_DSL = {
+    'default': {
+        'hosts':'localhost:9200'
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
