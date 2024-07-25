46dd94ac1854ab42934a5b3aeef8e4d53dfc8f21
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-07-25 11:10:04 +0400

    Adding db configs for postgres

--- MyShop/MyShop/settings.py
+++ MyShop/MyShop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "shop"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "MyShop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myshop_db",
+        "USER": "myshop_user",
+        "PASSWORD": "myshop_pass",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
