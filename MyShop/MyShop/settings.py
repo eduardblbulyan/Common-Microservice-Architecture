e5c9bf2dcb2cfa0a25f765275872294b8332c8ff
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-07-25 11:40:16 +0400

    Adding celery

--- MyShop/MyShop/settings.py
+++ MyShop/MyShop/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
